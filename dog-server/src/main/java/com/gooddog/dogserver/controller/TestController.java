fec8af569a3f28d5f61c50f610c24ca1a9f4443d
Haffer <1119728640@qq.com>
2021-05-07 18:06:42 +0800

    测试提交

--- dog-server/src/main/java/com/gooddog/dogserver/controller/TestController.java
+++ dog-server/src/main/java/com/gooddog/dogserver/controller/TestController.java
@@ -2,6 +2,7 @@ package com.gooddog.dogserver.controller;
 
 import cn.afterturn.easypoi.excel.ExcelImportUtil;
 import cn.afterturn.easypoi.excel.entity.ImportParams;
+import cn.hutool.core.text.StrBuilder;
 import com.gooddog.base.exception.ServiceException;
 import com.gooddog.base.resp.JsonProtocol;
 import com.gooddog.dogserver.po.InStockPo;
@@ -12,8 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.List;
 
 /**
@@ -43,6 +43,55 @@ public class TestController {
             throw new ServiceException("io异常");
         } catch (Exception e) {
             throw new ServiceException("解析异常");
+        }
+    }
+
+    @PostMapping("/genFile")
+    public void genFile(@RequestParam MultipartFile fileParam) {
+        StringBuilder strBuilder = new StringBuilder("\r\ninsert into table (`name`,`value`) value ");
+        BufferedReader bufferedReader = null;
+        PrintWriter printWriter = null;
+        StrBuilder text = StrBuilder.create();
+        String lineText;
+        try {
+            InputStream is = fileParam.getInputStream();
+            bufferedReader = new BufferedReader(new InputStreamReader(is));
+            while ( (lineText = bufferedReader.readLine()) != null) {
+                text.append(lineText);
+            }
+            bufferedReader.close();
+            if (text.length() > 0) {
+                String[] split = text.toString().split("，");
+                for (String s : split) {
+                    strBuilder.append("('").append(s).append("',1),\r\n");
+                }
+                // 最后一个,的索引
+                int index = strBuilder.toString().lastIndexOf(",");
+                strBuilder.replace(index, index + 1, ";");
+
+            }
+            // 本地是否存在此文件
+            File localFile = new File("D:/a.sql");
+            if (!localFile.exists()) {
+                localFile.createNewFile();
+            }
+            FileOutputStream fos = new FileOutputStream(localFile, true);
+            printWriter = new PrintWriter(fos);
+            printWriter.append(strBuilder);
+            printWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (printWriter != null) {
+                printWriter.close();
+            }
         }
     }
 
